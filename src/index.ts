c3b9ddd78474468a76d23e5b5f53bd6e91f2fd68
Przemysław Trzepiński <przemyslawtrzepinski@Macbook-Pro-Przemyslaw-T.local>
2019-12-15 16:56:55 +0100

    add: basic styling

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import m from "materialize-css";
+import "./styles/index.scss";
 
 window.addEventListener("load", () => {
   console.log({ m });
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,17 @@ var config = {
         test: /\.tsx?$/,
         loader: "ts-loader",
         exclude: /node_modules/
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
       }
     ]
   }
